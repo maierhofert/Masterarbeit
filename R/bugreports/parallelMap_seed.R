b7d2335a04f9e5ae06cd27d2c0000e1e1c42f447
maierhofert <thomasmaierhofer@hotmail.de>
2017-05-18 17:22:24 +0200

    check out how parallelMap sets seeds

---
+++ R/bugreports/parallelMap_seed.R
@@ -0,0 +1,15 @@
+
+library("parallelMap")
+
+# benchmark in parallel
+# setting a seed does not seem to work
+# set.seed(1234, "L'Ecuyer-CMRG")
+
+parallelMap::parallelStartSocket(cpus = 4)
+
+# you havce to set a seed here like this
+# otherwise it won´t work
+parallel::clusterSetRNGStream(iseed = 42)
+
+warning(parallelMap::parallelLapply(1:10, rnorm)[[10]])
+parallelStop()
