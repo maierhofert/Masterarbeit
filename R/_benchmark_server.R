dac5a06a88d977169fdcea8456b4a5cc37de114d
maierhofert <thomasmaierhofer@hotmail.de>
2017-05-18 17:19:33 +0200

    setup server for simulation study

--- R/_benchmark_server.R
+++ R/_benchmark_server.R
@@ -3,8 +3,9 @@ source("R/UCR_TSC_data.R")
 source("R/create_base_learners.R")
 source("R/tune_base_learners.R")
 source("R/tune_opt_knn_nderiv_learners.R")
-source("R/benchmark.R")
+# source("R/benchmark.R")
+source("R/benchmark_simulation.R")
 
 # save benchmarking result
-saveRDS(bmr, paste0(Sys.Date(), "bmr.RDS"))
-saveRDS(bmr, "bmr.RDS")
+saveRDS(bmr, paste0("Benchmark_results/", Sys.Date(), "bmr.RDS"))
+saveRDS(bmr, "Benchmark_results/bmr.RDS")
