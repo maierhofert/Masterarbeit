b487705aadd02a334bcd72067c07defa737e56e8
maierhofert <thomasmaierhofer@hotmail.de>
2017-06-06 21:35:08 +0200

    include date

--- R/_benchmark_server.R
+++ R/_benchmark_server.R
@@ -1,11 +1,12 @@
 # this file can be used to run the benchmarking on a server
+date()
 source("R/UCR_TSC_data.R")
 source("R/create_base_learners.R")
 source("R/tune_base_learners.R")
 source("R/tune_opt_knn_nderiv_learners.R")
 source("R/benchmark.R")
 # source("R/benchmark_simulation.R")
-
+date()
 # save benchmarking result
-saveRDS(bmr, paste0("Benchmark_results/", Sys.Date(), "_simu_trigonometric_bmr.RDS"))
+saveRDS(bmr, paste0("Benchmark_results/", Sys.Date(), "parallel_resample_bmr.RDS"))
 saveRDS(bmr, "Benchmark_results/bmr.RDS")
