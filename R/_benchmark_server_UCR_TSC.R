fb759213600f0c0c2f6882da350d4204e2eb0771
maierhofert <thomasmaierhofer@hotmail.de>
2017-06-09 13:29:48 +0200

    create benchmark_server files

---
+++ R/_benchmark_server_UCR_TSC.R
@@ -0,0 +1,13 @@
+# this file can be used to run the benchmarking on a server
+date()
+source("R/UCR_TSC_data.R")
+source("R/create_base_learners.R")
+source("R/tune_base_learners.R")
+source("R/tune_opt_knn_nderiv_learners.R")
+source("R/benchmark.R")
+# source("R/benchmark_simulation.R")
+# source("R/benchmark_simulation_warped.R")
+date()
+# save benchmarking result
+saveRDS(bmr, paste0("Benchmark_results/", Sys.Date(), "UCR_TSC_bmr.RDS"))
+saveRDS(bmr, "Benchmark_results/bmr.RDS")
