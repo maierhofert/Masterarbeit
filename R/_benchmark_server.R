60cc6a10c025cd67a4958b62dcf7a938f0eb440c
maierhofert <thomasmaierhofer@hotmail.de>
2017-07-14 10:40:02 +0200

    cleanup

--- R/_benchmark_server.R
+++ R/_benchmark_server.R
@@ -5,8 +5,6 @@ source("R/create_base_learners.R")
 # source("R/tune_base_learners.R")
 source("R/create_opt_base_learners.R")
 source("R/benchmark.R")
-# source("R/benchmark_simulation.R")
-# source("R/benchmark_simulation_warped.R")
 date()
 # save benchmarking result
 saveRDS(bmr, paste0("Benchmark_results/", Sys.Date(), "bmr.RDS"))
