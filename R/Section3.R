e564c41f883d7d632722d68960738eeb7c5727c7
maierhofert <thomasmaierhofer@hotmail.de>
2017-10-30 16:04:12 -0700

    fix section 3, satrt section 4

--- R/Section3.R
+++ R/Section3.R
@@ -68,6 +68,7 @@ classiKernel(classes, fdata, grid = 1:ncol(fdata), h = 1,
 # table(pred = pred1, true = Growth$sex[test_ids])
 # table(pred = pred2, true = Growth$sex[test_ids])
 
+
 ################################################################################
 # install the package once
 install.packages("classiFunc")
@@ -77,31 +78,62 @@ library("classiFunc")
 
 # load the example data
 data("DTI", package = "classiFunc")
+# check out help file for DTI data
+?DTI
 
-# determine number of observations N
-IDs = unique(DTI$ID)
-
-# randomly assign data to test and training data
+# randomly assign participant IDs into test and training data
 set.seed(1234)
-train_ids = sample(IDs, size = 0.9 * length(IDs))
-# vector to check if observation is part of test or training
+IDs = unique(DTI$ID)
+train_ids = sample(IDs, size = 0.95 * length(IDs))
+# vector encoding if observation is part of test or training data
 train_rows = DTI$ID %in% train_ids
 
 # create nearest neighbor estimator with default values
 nn_mod = classiKnn(classes = DTI$case[train_rows], 
-                    fdata = DTI$rcst[train_rows,])
+                   fdata = DTI$rcst[train_rows,])
 
-# create Epanechnikov kernel estimator with manually selected bandwidth
+# create Epanechnikov kernel estimator for first order derivatives
+# with manually set bandwidth
 ker_mod = classiKernel(classes = DTI$case[train_rows], 
                        fdata = DTI$rcst[train_rows,],
                        ker = "Ker.epa",
-                       h = 0.8)
+                       h = 0.3, 
+                       nderiv = 1)
 # Chunk 3
 # predict the nearest neighbor estimators
+# hyperparameters (k, h, ker, nderiv, ...) are stored in the model,
+# they do not have to be specified again
 pred_nn = predict(nn_mod, newdata = DTI$rcst[!train_rows,])
 pred_ker = predict(ker_mod, newdata = DTI$rcst[!train_rows,])
 
 # Chunk 4
+# confusion matrix for nn estimator
 table(pred = pred_nn, true = DTI$case[!train_rows])
+# confusion matrix for kernel estimator
 table(pred = pred_ker, true = DTI$case[!train_rows])
 ################################################################################
+
+### Chapter 4, usage through mlr-interface
+# Chunk 4.1
+# install the mlr package once
+install.packages("mlr")
+devtools::install_github("maierhofert/mlr", ref = "classiFunc")
+
+# load the package in every new R-session
+library("mlr")
+
+# load the example data
+data("DTI", package = "classiFunc")
+
+# export DTI data to mlr data format
+fdata = makeFunctionalData(data = DTI, fd.features = list(rcst = "rcst", cca = "cca"))
+task = makeClassifTask(data = fdata, target = "case")
+
+
+
+
+
+
+
+
+
